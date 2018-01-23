801020b9cc3cdebcf47ffc932e1faa7a7c4c04a5
kslovic <kslovic@etfos.hr>
2018-01-23 21:02:07 +0100

    database config

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host'      => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'heroku_9fac3169743c64c'),
+            'username'  => env('DB_USERNAME', 'bf7c1d8b79b38d'),
+            'password'  => env('DB_PASSWORD', 'b5c9e9ab'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
