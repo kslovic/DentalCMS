adaa96e0c4f2b0bcfd47822b2811d3ffb4cd4d00
kslovic <kslovic@etfos.hr>
2018-01-24 03:57:47 +0100

    scheduler11

--- app/Console/Commands/SchedulerDaemon.php
+++ app/Console/Commands/SchedulerDaemon.php
@@ -32,7 +32,7 @@ class SchedulerDaemon extends Command
         while (true) {
             $this->line('<info>[' . Carbon::now()->format('Y-m-d H:i:s') . ']</info> Calling scheduler');
 
-            $this->exec('php artisan schedule:run');
+            $this->exec('heroku run php artisan schedule:run');
 
             sleep($this->option('sleep'));
         }
